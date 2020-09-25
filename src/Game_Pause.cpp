fe472f4f57eaf09b3f34b3ba826fee13816f0f76
Filmote <simon@bloggingadeadhorse.com>
2020-09-25 22:36:02 +1000

    Rotating

--- src/Game_Pause.cpp
+++ src/Game_Pause.cpp
@@ -9,6 +9,19 @@ constexpr const static uint8_t UPLOAD_DELAY = 16;
 
 int16_t xCass = 0;
 int16_t yCass = 0;
+int16_t pathIdx[3];
+int16_t pathDir = 0;
+int16_t pathCounter = 0;
+
+const uint8_t xPath[84] = {  0,  2,  4,  6,  8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82,  
+                            82, 80, 78, 76, 74, 72, 70, 68, 66, 64, 62, 60, 58, 56, 54, 52, 50, 48, 46, 44, 42, 40, 38, 36, 34, 32, 30, 28, 26, 24, 22, 20, 18, 16, 14, 12, 10,  8,  6,  4,  2,  0 };
+
+const uint8_t yPath[84] = { 10, 12, 14, 14, 15, 16, 16, 17, 17, 17, 18, 18, 18, 18, 18, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 18, 18, 18, 18, 18, 17, 17, 17, 16, 16, 15, 14, 14, 12, 
+                            10,  8,  6,  6,  5,  4,  4,  3,  3,  3,  2,  2,  2,  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  3,  3,  3,  4,  4,  5,  6,  6,  8 };
+
+const uint8_t zPath[84] = {  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,
+                             2,  2,  2,  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, 1,  1,  1,  1,  1,  };
+
 
 // ----------------------------------------------------------------------------
 //  Initialise state ..
@@ -18,6 +31,28 @@ void Game::doPause_Init() {
     this->gameState = GameState::Pause;
     this->cursor = this->cookie->track;
 
+    switch (this->cookie->track) {
+
+        case 1:
+            pathIdx[0] = 49;
+            pathIdx[1] = pathIdx[0] - 28;
+            pathIdx[2] = pathIdx[0] + 28;
+            break;
+
+        case 2:
+            pathIdx[1] = 49;
+            pathIdx[2] = pathIdx[1] - 28;
+            pathIdx[0] = pathIdx[1] + 28;
+            break;
+
+        case 0:
+            pathIdx[2] = 49;
+            pathIdx[0] = pathIdx[2] - 28;
+            pathIdx[1] = pathIdx[2] + 28;
+            break;
+
+    }
+
 }
 
 
@@ -26,19 +61,20 @@ void Game::doPause_Init() {
 //
 void Game::doPause() {
 	
-    if (xCass == 0) {
+    if (xCass == 0 && pathCounter == 0) {
 
-        if (PC::buttons.pressed(BTN_UP)) {
-            if (this->cursor == 0) {
-                this->cursor = 2;
-            }
-            else {
-                this->cursor = (this->cursor - 1) % 3;
-            }
+        if (PC::buttons.pressed(BTN_LEFT)) {
+
+            pathDir = -1;
+            pathCounter = 28;
+
         }
 
-        if (PC::buttons.pressed(BTN_DOWN)) {
-            this->cursor = (this->cursor + 1) % 3;
+        if (PC::buttons.pressed(BTN_RIGHT)) {
+
+            pathDir = 1;
+            pathCounter = 28;
+
         }
 
         if (PC::buttons.pressed(BTN_C)) {
@@ -50,21 +86,21 @@ void Game::doPause() {
             if (this->cookie->sfx == SoundEffects::None) {
 
                 this->cookie->sfx = SoundEffects::Music;
-                xCass = 86;
-                yCass = 64 + (this->cursor * 12);
+                xCass = 79;
+                yCass = 93;
 
             }
             else if (this->cookie->sfx == SoundEffects::SFX) {
 
                 this->cookie->sfx = SoundEffects::Both;
-                xCass = 86;
-                yCass = 64 + (this->cursor * 12);
+                xCass = 79;
+                yCass = 93;
 
             }
             else if (this->cookie->track != this->cursor) {
 
-                xCass = 86;
-                yCass = 64 + (this->cursor * 12);
+                xCass = 79;
+                yCass = 93;
 
             }
 
@@ -72,21 +108,68 @@ void Game::doPause() {
 
     }
 
+    if (pathCounter > 0) {
+
+        pathCounter--;
+
+        for (uint8_t i = 0; i < 3; i++) {
+
+            pathIdx[i] = pathIdx[i] + pathDir;
+
+            if (pathIdx[i] < 0) {
+                pathIdx[i] = pathIdx[i] + 84;
+            }
+
+            if (pathIdx[i] >= 84) {
+                pathIdx[i] = pathIdx[i] - 84;
+            }
+
+        }
+
+        if (pathCounter == 0) {
+
+            this->cursor = this->cursor + pathDir;
+
+            if (this->cursor == 255) this->cursor = 2;
+            if (this->cursor == 3) this->cursor = 0;
+            
+        }
+
+    }
+
 
     if (xCass > 0) {
 
-        if (xCass <= 162) {
-            xCass = xCass + 4;
-        }
-        else if (xCass <= 164) {
-            xCass = xCass + 2;
+        switch (xCass) {
+
+            case 0 ... 162:
+                xCass = xCass + 4;
+                break;
+
+            case 163:
+                xCass = xCass + 2;
+                break;
+
+            case 164 ... 165:
+                xCass = xCass + 1;
+                break;
+
         }
 
-        if (yCass > 6) {
-            yCass = yCass - 4;
-        }
-        else if (yCass >= 4) {
-            yCass = yCass - 2;
+        switch (yCass) {
+
+            case 3:
+                yCass = yCass - 1;
+                break;
+
+            case 4 ... 6:
+                yCass = yCass - 2;
+                break;
+
+            case 7 ... 255:
+                yCass = yCass - 4;
+                break;
+
         }
 
         if (yCass == 2 && xCass == 166) {
@@ -111,17 +194,31 @@ void Game::doPause() {
 
     PD::drawBitmap(95, 46, Images::PausePlay);
 
+    if (pathCounter == 0) {
 
-    for (uint8_t i = 0; i < 3; i++) {
+        PD::drawBitmap(40, 86, Images::ArrowLeft);
+        PD::drawBitmap(168, 86, Images::ArrowRight);
 
-        if (xCass > 0 && i == this->cursor) {
+    }
 
-            PD::drawBitmap(xCass, yCass, Images::Tapes[this->cursor][0]);
+    for (uint8_t z = 0; z < 3; z++) {
 
-        }
-        else {
+        for (uint8_t i = 0; i < 3; i++) {
 
-            PD::drawBitmap(80 + (this->cursor == i ? 6: 0), 64 + (i * 12), Images::Tapes[i][0]);
+            if (zPath[pathIdx[i]] == z) {
+
+                if (xCass > 0 && i == this->cursor) {
+
+                    PD::drawBitmap(xCass, yCass, Images::Tapes_Shadow[this->cursor]);
+
+                }
+                else {
+
+                    PD::drawBitmap(41 + xPath[pathIdx[i]], 70 + yPath[pathIdx[i]], Images::Tapes_Shadow[i]);
+
+                }
+
+            }
 
         }
 
--- src/images/tapes/Tapes.h
+++ src/images/tapes/Tapes.h
@@ -5,16 +5,21 @@ namespace Images {
     #include "BlueTape_00.h"
     #include "BlueTape_01.h"
     #include "BlueTape_02.h"
+    #include "BlueTape_Shadow.h"
 
     #include "GreenTape_00.h"
     #include "GreenTape_01.h"
     #include "GreenTape_02.h"
+    #include "GreenTape_Shadow.h"
 
     #include "RedTape_00.h"
     #include "RedTape_01.h"
     #include "RedTape_02.h"
+    #include "RedTape_Shadow.h"
 
     #include "Pause.h"
+    #include "ArrowLeft.h"
+    #include "ArrowRight.h"
 
     const uint8_t * const Tapes[3][3] = { 
         GreenTape_00,
@@ -26,6 +31,12 @@ namespace Images {
         BlueTape_00,
         BlueTape_01,
         BlueTape_02,
+    };
+
+    const uint8_t * const Tapes_Shadow[3] = { 
+        GreenTape_Shadow,
+        RedTape_Shadow,
+        BlueTape_Shadow,
     };
 
 };
